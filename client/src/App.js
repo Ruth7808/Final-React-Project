81140b805976e3b9181222b9d58d6e9f8bf6c164
ruth7808 <ruth0583244968@gmail.com>
2024-05-07 18:03:13 +0300

    added h2 in local machine

--- client/src/App.js
+++ client/src/App.js
@@ -14,6 +14,7 @@ import Conclude from './features/users/Conclude';
 function App() {
   return (
     <>
+    
      <Routes path='/' element={<Home/>}>
           <Route index element={<Home/>} />
           <Route path='/user/products' element={<ProductsList />} />
